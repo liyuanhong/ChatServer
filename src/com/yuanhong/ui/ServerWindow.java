d9a69dd9ce31e5b0ccfa738fb5ada3bc9aae0f78
liyuanhong <liyh@witmob.com>
2013-06-01 22:45:04 +0800

    create an window for server

---
+++ src/com/yuanhong/ui/ServerWindow.java
@@ -0,0 +1,43 @@
+package com.yuanhong.ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class ServerWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ServerWindow window = new ServerWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ServerWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
